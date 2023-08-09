0eedef7fb7a91a9bd71e032ed2cd6d6c0fcfd65e
Chris_Vlachos <cv@mcinnovations.com.gr>
2023-08-09 23:12:04 +0300

    Side-Effects > useReducer() (to manage email state and login)

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,43 +1,60 @@
-import React, { useEffect, useState } from 'react';
+import React, { useReducer, useState } from 'react';
+// import { useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
 
+const emailReducer = (state, action) => {
+  if (action.type === 'USER_INPUT') {
+    // we the 'action' returned by the 'emailChangeHandler' and
+    // we return a new state snapshot for emailState
+    return { value: action.val, isValid: action.val.includes('@') }
+  }
+  if (action.type === 'INPUT_BLUR') {
+    // we access the value of the email from the last state snapshot 'state'
+    // see the 'validateEmailHandler' we don't return a val so we cannot
+    // access the value through there...that's why React gives us the last
+    // state snapshot in the 'state' input param.
+    return { value: state.value, isValid: state.value.includes('@') }
+  }
+  return { value: '', isValid: null }
+}
+
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
-  const [emailIsValid, setEmailIsValid] = useState();
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [emailIsValid, setEmailIsValid] = useState();
   const [enteredPassword, setEnteredPassword] = useState('');
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
-  useEffect(() => {
-
-    const identifier = setTimeout(() => {
-      console.log('Checking form validity-useEffect')
-      setFormIsValid(
-        enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      );
-    }, 500);
-
-    // CleanUp function - will run before the useEffect function execution (but not the 1st time)
-    return () => {
-      console.log('CLEANUP')
-      clearTimeout(identifier)
-    }
-    
-  }, [enteredEmail, enteredPassword])
+  const [emailState, dispatchEmail] = useReducer(emailReducer, {
+    value: '',
+    isValid: false
+  })
 
   const emailChangeHandler = (event) => {
-    setEnteredEmail(event.target.value);   
+    // setEnteredEmail(event.target.value);
+    // this will trigger the  reducers function i.e. emailReducer
+    // the below object will be the 'action' input param in emailReducer
+    dispatchEmail({ type: 'USER_INPUT', val: event.target.value })
+
+    setFormIsValid(
+      event.target.value.includes('@') && enteredPassword.trim().length > 6
+    )
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+
+    setFormIsValid(
+      emailState.isValid && event.target.value.trim().length > 6
+    )
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    // setEmailIsValid(emailState.isValid);
+    dispatchEmail({ type: 'INPUT_BLUR' })
   };
 
   const validatePasswordHandler = () => {
@@ -46,21 +63,21 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(enteredEmail, enteredPassword);
+    props.onLogin(emailState.value, enteredPassword);
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <div
-          className={`${classes.control} ${emailIsValid === false ? classes.invalid : ''
+          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
             }`}
         >
           <label htmlFor="email">E-Mail</label>
           <input
             type="email"
             id="email"
-            value={enteredEmail}
+            value={emailState.value}
             onChange={emailChangeHandler}
             onBlur={validateEmailHandler}
           />
