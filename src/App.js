904719c6953faf963cb049712a44ceeeaeee89ec
rnd2 <cv@mcinnovations.com.gr>
2023-08-29 11:08:46 +0300

    memo & useCallback for unnecessary re-evaluations based on props and functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import Button from './components/UI/Button/Button'
 import './App.css';
 import DemoOutput from './components/UI/Button/Demo/DemoOutput';
@@ -8,15 +8,16 @@ function App() {
 
   console.log('APP RUNNING')
 
-  const handleToggleParagraph = () => {
+  const handleToggleParagraph = useCallback(() => {
     setShowParagraph((prevShowParagraph) => {
       return !prevShowParagraph
     })
-  }
+  }, [])
   return (
     <div className="app">
       <h1>Hi there!</h1>
       <DemoOutput show={showParagraph} />
+      {/* {showParagraph && <MyParagraph>This is new!</MyParagraph>} */}
       <Button onClick={handleToggleParagraph}>Toggle Paragraph!</Button>
     </div>
   );
