94e4f7bd77b27a35eeea18f1f527e8623f610726
rnd2 <cv@mcinnovations.com.gr>
2023-09-04 14:03:23 +0300

    POST request with our custom hook

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,34 @@
-import { useState } from 'react';
-
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp()
+
+  const createTask = (taskText, data) => {
+    const generatedId = data.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+    props.onAddTask(createdTask);
+  }
+
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-testing-2f51c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
+    sendTaskRequest(
+      {
+        url: 'https://react-http-testing-2f51c-default-rtdb.europe-west1.firebasedatabase.app/tasks.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { text: taskText }
+      },
+      // we use bind because the createTask takes 2 args but the 'applyData'
+      // in sendRequest in use-http.js takes only 1 argument
+      createTask.bind(null, taskText)
+      // another solution would be to put the 'createTask' func inside the 
+      // 'enterTaskHandler' and not use the bind
+    )
   };
 
   return (
