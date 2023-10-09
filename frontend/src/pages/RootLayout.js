04775a30aaf83bfb1c3d0d04492788354eb6f90a
Chris_Vlachos <cv@mcinnovations.com.gr>
2023-10-09 22:09:37 +0300

    Reflecting the current/loading state in the UI

--- frontend/src/pages/RootLayout.js
+++ frontend/src/pages/RootLayout.js
@@ -1,11 +1,14 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useNavigation } from 'react-router-dom'
 import MainNavigation from '../components/MainNavigation'
 
 const RootLayout = () => {
+    const navigation = useNavigation()
+
     return <>
         <h1>RootLayout</h1>
         <MainNavigation />
         <main>
+            {navigation.state === 'loading' && <p>Loading...</p>}
             <Outlet />
             {/* to define where the content of the child routes
             should be rendered (see App.js for the routess) */}
