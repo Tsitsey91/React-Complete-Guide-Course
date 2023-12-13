2287de1fa27fe688c57b8f51d5d7936d51c30def
Chris_Vlachos <cv@mcinnovations.com.gr>
2023-12-13 20:54:44 +0200

    Creating our first state Slice!

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,32 @@
 // Create a store. Create a reducer for the counter
 
 import { createStore } from "redux"
+import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = { counter: 0, showCounter: true }
+
+// we create a slice(a subset) of our global state(we can create many 
+// slices in different files)
+createSlice({
+    name: 'counterSlice',
+    initialState: initialState,
+    // the reduces below are equiv to the "counterReducer" below but they use redux-toolkit(less boilerplate)
+    reducers: {
+        increment(state) {
+            state.counter++ // with redux-toolkit we can mutate the state(under the hood the state obj is copied and a new one is returned, so we dont mutate it...its just redux-toolkit that takes care of this)
+        },
+        decrement(state) {
+            state.counter--
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter
+        },
+    }
+})
+
 const counterReducer = (currentState = initialState, action) => {
     // NEVER MUTATE THE EXISTING STATE-IT COULD LEAD TO UNPREDICTABLE BUGS
     // I.E. NEVER DO SOMETHING LIKE currentState.counter++ and then return it
